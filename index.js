961fecdba0cff69a8b948986ac079b4f430a4cea
Sanjaee <afrizaahmad18@gmail.com>
2025-06-24 23:58:14 +0700

    test

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "https://lost-media.vercel.app" ,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-User-Role"],
